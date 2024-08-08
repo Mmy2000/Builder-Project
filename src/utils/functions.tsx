afd2d6bec2632d1f6c2acd5a6650734bfb7a8e6b
Mmy2000 <yahianada005@gmail.com>
2024-08-09 00:25:29 +0300

    displaying data

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {string} text - the input must be sliced.
+ * @param {number} [max = 50] - the max length before truncation.
+ * @returns - the sliced text, with an ellipsis (...) appended if sliced
+ */
 export function textSlicer(text:string , max:number = 50){
     if (text.length >= max) {
         return `${text.slice(0, max)} ...`;
