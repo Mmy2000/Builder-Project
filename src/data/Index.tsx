08c4140c90ddba60b59c465de4baa33742bf3d58
Mmy2000 <yahianada005@gmail.com>
2024-08-08 23:43:55 +0300

    displaying data

---
+++ src/data/Index.tsx
@@ -0,0 +1,110 @@
+import {IProduct} from '../interfaces'
+import { v4 as uuidv4 } from "uuid";
+
+export const productList: IProduct[] = [
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+  {
+    id: uuidv4(),
+    title: "2022 Genesis GV70",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi.",
+    price: "500000",
+    category: {
+      name: "cars",
+      imageURL:
+        "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    },
+    imageURL:
+      "https://img.freepik.com/premium-photo/modern-automobile-classic-technology-wheel-traffic_665346-119.jpg",
+    colors: ["#FF0032", "#2563eb", "#FF6E31"],
+  },
+];
