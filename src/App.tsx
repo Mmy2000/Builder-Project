f8f2d51bdc1637f83bccc81b5d1b8022479d1a52
Mmy2000 <yahianada005@gmail.com>
2024-08-09 05:34:09 +0300

    create modal

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,18 @@ function App() {
   ));
   const renderedFormInput = formInputList.map((input) => (
     <div className="flex flex-col">
-      <label htmlFor={input.id}>{input.label}</label>
+      <label htmlFor={input.id} className=''>{input.label}</label>
       <Input id={input.id} name={input.name} type='text' />
     </div>
   ));
   return (
     <>
       <div className="px-5 container mx-auto my-10  ">
-        <Buttons className="bg-green-600 mb-6 text-white font-medium hover:bg-green-800" width='w-fit' onClick={open}>
+        <Buttons
+          className="bg-green-600 mb-6 text-white font-medium hover:bg-green-800"
+          width="w-fit"
+          onClick={open}
+        >
           Build Now
         </Buttons>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6">
@@ -37,14 +41,16 @@ function App() {
         </div>
       </div>
       <Modal isOpen={isOpen} closeModal={close} title="Add New Product">
-        {renderedFormInput}
-        <div className="flex justify-between mt-5 gap-20">
-          <Buttons className=" bg-green-600 text-white font-medium hover:bg-green-800 ">
-            Submit
-          </Buttons>
-          <Buttons className=" bg-red-600 text-white font-medium hover:bg-red-800 ">
-            Cancel
-          </Buttons>
+        <div className='space-y-2'>
+          {renderedFormInput}
+          <div className="flex justify-between mt-5 gap-2">
+            <Buttons className=" bg-green-600 text-white font-medium hover:bg-green-800 ">
+              Submit
+            </Buttons>
+            <Buttons className=" bg-red-600 text-white font-medium hover:bg-red-800 ">
+              Cancel
+            </Buttons>
+          </div>
         </div>
       </Modal>
     </>
