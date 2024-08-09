d19bcf31685e1d50fca84092f0376ab2e4e04c71
Mmy2000 <yahianada005@gmail.com>
2024-08-09 03:47:16 +0300

    create modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 
+import { useState } from 'react';
 import ProductCard from './components/ProductCard'
 import Modal from './components/ui/Modal';
 import { productList } from './data/Index';
+import Buttons from './components/ui/Buttons';
+
 
 function App() {
-     const renderedProductList = productList.map((product) => (
-       <ProductCard product={product} key={product.id} />
-     ));
+  const [isOpen, setIsOpen] = useState(true);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+  const renderedProductList = productList.map((product) => (
+    <ProductCard product={product} key={product.id} />
+  ));
   return (
     <>
       <div className="px-5 container mx-auto my-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 ">
         {renderedProductList}
       </div>
-      <Modal />
+      <Modal isOpen={isOpen} closeModal={close} title="Hi">
+        <div className="flex justify-between gap-20">
+          <Buttons className=" bg-green-600 text-white font-medium hover:bg-green-800 ">
+            Submit
+          </Buttons>
+          <Buttons className=" bg-red-600 text-white font-medium hover:bg-red-800 ">
+            Cancel
+          </Buttons>
+        </div>
+      </Modal>
     </>
   );
 }
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,60 +1,44 @@
 import {  Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
-import { useState } from "react";
+import { ReactNode } from "react";
 import Buttons from "./Buttons";
 
-export default function Modal() {
-  const [isOpen, setIsOpen] = useState(true);
 
-  function open() {
-    setIsOpen(true);
-  }
+interface IProps {
+  isOpen : boolean,
+  closeModal : ()=> void,
+  title? : string , 
+  children : ReactNode
 
-  function close() {
-    setIsOpen(false);
-  }
+}
+
+export default function Modal({isOpen , closeModal , title , children}:IProps) {
+  
 
   return (
     <>
-      <Buttons
-        onClick={open}
-        className="rounded-md bg-black/20 py-2 px-4 text-sm font-medium text-black focus:outline-none data-[hover]:bg-black/30 data-[focus]:outline-1 data-[focus]:outline-white"
-        width="w-fit"
-      >
-        Open dialog
-      </Buttons>
-
       <Dialog
         open={isOpen}
         as="div"
         className="relative z-10 focus:outline-none"
-        onClose={close}
+        onClose={closeModal}
         __demoMode
       >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
+        <div className="fixed  inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex  min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-md rounded-xl bg-white/5 shadow-xl p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
-              <DialogTitle
-                as="h3"
-                className="text-base/7 font-medium text-black"
-              >
-                Payment successful
-              </DialogTitle>
-              <p className="mt-2 text-sm/6 text-black/50">
-                Your payment has been successfully submitted. We’ve sent you an
-                email with all of the details of your order.
-              </p>
-              <div className="mt-4">
-                <Buttons
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                  onClick={close}
-                  width="w-fit"
+              {title && (
+                <DialogTitle
+                  as="h3"
+                  className="text-base/7 font-medium text-black"
                 >
-                  Got it, thanks!
-                </Buttons>
-              </div>
+                  {title}
+                </DialogTitle>
+              )}
+
+              <div className="mt-4">{children}</div>
             </DialogPanel>
           </div>
         </div>
