4aa0d993b66dc0959162640a6f0c3543bf907e27
Mmy2000 <yahianada005@gmail.com>
2024-08-12 01:07:42 +0300

    adding validation

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,7 +5,7 @@ interface IProps extends InputHTMLAttributes<HTMLInputElement> {}
 export default function Input({ ...rest }: IProps) {
   return (
     <input
-      className={`px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 `}
+      className={`px-4 py-2 border shadow-md rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 `}
       {...rest}
     />
   );
---
+++ src/validation/index.ts
@@ -0,0 +1,20 @@
+
+
+
+export const productValidation = (product : {title:string , description: string})=>{
+    const errors:{title: string ,description: string ,imageURL:string ,price: string} = {
+        title: "",
+        description: "",
+        imageURL: "",
+        price: "",
+    }
+    if (product.title.trim() || product.title.length < 10 || product.title.length > 80) {
+        errors.title = "Product title must be between 10 and 80 characters!"
+    }
+    if (product.description.trim() || product.description.length < 10 || product.description.length > 80) {
+        errors.title = "Product description must be between 10 and 900 characters!"
+    }
+
+
+    return errors
+}
